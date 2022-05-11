d719a8a70d8098776d32efa129289b7ccd27d3e6
duhesme <hitechwatchout@gmail.com>
2022-05-11 22:38:07 +0300

    Initial commit

---
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    print(e)
+                    let alert = UIAlertController(title: "Ошибка", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+             
+            }
+        }
+    }
+    
+}
---
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    let alert = UIAlertController(title: "Ошибка", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+        }
+            
+    }
+    
+}
